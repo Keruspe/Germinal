aeaad4c0c0c33cbc4f2033e8dc01ec88e5538965
Marc-Antoine Perennou <Marc-Antoine@Perennou.com>
2015-09-07 17:45:56 +0200

    trivial code cleanup
    Signed-off-by: Marc-Antoine Perennou <Marc-Antoine@Perennou.com>

--- src/germinal.c
+++ src/germinal.c
@@ -39,8 +39,8 @@ on_child_exited (VteTerminal *vteterminal,
 }
 
 static gboolean
-do_copy (GtkWidget *widget    G_GNUC_UNUSED,
-         gpointer   user_data G_GNUC_UNUSED)
+do_copy (GtkWidget *widget G_GNUC_UNUSED,
+         gpointer   user_data)
 {
     vte_terminal_copy_clipboard (VTE_TERMINAL (user_data));
     return TRUE;
@@ -59,17 +59,17 @@ get_url (VteTerminal    *terminal,
          GdkEventButton *button_event)
 {
     static gchar *url = NULL; /* cache the url */
+
     if (button_event) /* only access to cached url if no button_event available */
     {
         g_free (url); /* free previous url */
         glong column = (glong)button_event->x / vte_terminal_get_char_width (terminal);
         glong row = (glong)button_event->y / vte_terminal_get_char_height (terminal);
         gint tag; /* avoid stupid vte segv (said to be optional) */
-        url = vte_terminal_match_check (terminal,
-                                        column,
-                                        row,
-                                        &tag);
+
+        url = vte_terminal_match_check (terminal, column, row, &tag);
     }
+
     return url;
 }
 
@@ -84,10 +84,13 @@ open_url (gchar *url)
     g_autofree gchar *browser = g_strdup (g_getenv ("BROWSER"));
 
     /* If BROWSER is not in env, try xdg-open or fallback to firefox */
-    if (!browser && !(browser = g_find_program_in_path ("xdg-open")))
+    if (!browser)
+        browser = g_find_program_in_path ("xdg-open");
+    if (!browser)
         browser = g_strdup ("firefox");
 
     gchar *cmd[] = {browser, url, NULL};
+
     if (!g_spawn_async (NULL, /* working directory */
                         cmd,
                         NULL, /* env */
@@ -101,17 +104,27 @@ open_url (gchar *url)
     return TRUE;
 }
 
+static void
+copy_text (GdkAtom      selection,
+           const gchar *text)
+{
+    GtkClipboard *clip = gtk_clipboard_get (selection);
+
+    gtk_clipboard_set_text (clip, text, -1);
+}
+
 static gboolean
 do_copy_url (GtkWidget *widget G_GNUC_UNUSED,
              gpointer   user_data)
 {
     gchar *url = get_url (VTE_TERMINAL (user_data), NULL);
+
     if (!url)
         return FALSE;
-    GtkClipboard *clip = gtk_clipboard_get (GDK_SELECTION_CLIPBOARD);
-    gtk_clipboard_set_text (clip, url, -1);
-    clip = gtk_clipboard_get (GDK_SELECTION_PRIMARY);
-    gtk_clipboard_set_text (clip, url, -1);
+
+    copy_text (GDK_SELECTION_CLIPBOARD, url);
+    copy_text (GDK_SELECTION_PRIMARY, url);
+
     return TRUE;
 }
 
@@ -134,8 +147,10 @@ update_font_size (VteTerminal  *terminal,
                   FontSizeDelta delta)
 {
     static gdouble default_size = 0;
-    PangoFontDescription GERMINAL_FONT_CLEANUP *font = pango_font_description_copy (vte_terminal_get_font (terminal));
+
+    GERMINAL_FONT_CLEANUP PangoFontDescription *font = pango_font_description_copy (vte_terminal_get_font (terminal));
     gdouble size = pango_font_description_get_size (font);
+
     switch (delta)
     {
     case FONT_SIZE_DELTA_SET_DEFAULT:
@@ -148,6 +163,7 @@ update_font_size (VteTerminal  *terminal,
         size = CLAMP (size / PANGO_SCALE + delta, 4., 144.) * PANGO_SCALE;
         break;
     }
+
     pango_font_description_set_size (font, size);
     vte_terminal_set_font (terminal, font);
 }
@@ -157,6 +173,7 @@ do_zoom (GtkWidget *widget G_GNUC_UNUSED,
          gpointer   user_data)
 {
     update_font_size (VTE_TERMINAL (user_data), FONT_SIZE_DELTA_INC);
+
     return TRUE;
 }
 
@@ -165,6 +182,7 @@ do_dezoom (GtkWidget *widget G_GNUC_UNUSED,
            gpointer   user_data)
 {
     update_font_size (VTE_TERMINAL (user_data), FONT_SIZE_DELTA_DEC);
+
     return TRUE;
 }
 
@@ -173,14 +191,16 @@ do_reset_zoom (GtkWidget *widget G_GNUC_UNUSED,
                gpointer   user_data)
 {
     update_font_size (VTE_TERMINAL (user_data), FONT_SIZE_DELTA_RESET);
+
     return TRUE;
 }
 
 static gboolean
-do_quit (GtkWidget *widget    G_GNUC_UNUSED,
-         gpointer   user_data G_GNUC_UNUSED)
+do_quit (GtkWidget *widget,
+         gpointer   user_data)
 {
-    gtk_main_quit ();
+    germinal_exit (widget, user_data);
+
     return TRUE;
 }
 
@@ -257,11 +277,12 @@ on_button_press (GtkWidget      *widget,
         return FALSE;
 
     gchar *url = get_url (VTE_TERMINAL (widget), button_event);
+
     /* Shift + Left clic */
-    if ((button_event->button == 1) &&
+    if ((button_event->button == GDK_BUTTON_PRIMARY) &&
         (button_event->state & GDK_SHIFT_MASK))
             return open_url (url);
-    else if (button_event->button == 3)
+    else if (button_event->button == GDK_BUTTON_SECONDARY)
     {
         if (url) /* show url stuff */
             gtk_widget_show_all (GTK_WIDGET (user_data));
@@ -296,7 +317,7 @@ get_setting (GSettings   *settings,
 static GdkRGBA *
 get_palette (GSettings   *settings,
              const gchar *name,
-             gsize *palette_size)
+             gsize       *palette_size)
 {
     g_auto (GStrv) colors = NULL;
     guint size, i;
@@ -304,6 +325,7 @@ get_palette (GSettings   *settings,
 
     colors = g_settings_get_strv (settings, name);
     size = g_strv_length (colors);
+
     if (!((size == 0) ||
           (size == 8) ||
           (size == 16) ||
@@ -315,10 +337,12 @@ get_palette (GSettings   *settings,
     }
 
     palette = g_new(GdkRGBA, size);
+
     for (i = 0 ; i < size ; ++i)
         gdk_rgba_parse (&palette[i], colors[i]);
 
     *palette_size = size;
+
     return palette;
 }
 
@@ -346,15 +370,17 @@ update_font (GSettings   *settings,
              gpointer     user_data)
 {
     g_autofree gchar *setting = get_setting (settings, key);
-    PangoFontDescription GERMINAL_FONT_CLEANUP *font = pango_font_description_from_string (setting);
-    vte_terminal_set_font (VTE_TERMINAL (user_data), font);
-    update_font_size (VTE_TERMINAL (user_data), FONT_SIZE_DELTA_SET_DEFAULT);
+    GERMINAL_FONT_CLEANUP PangoFontDescription *font = pango_font_description_from_string (setting);
+    VteTerminal *term = user_data;
+
+    vte_terminal_set_font (term, font);
+    update_font_size (term, FONT_SIZE_DELTA_SET_DEFAULT);
 }
 
 static GdkRGBA *
 update_colors (GSettings   *settings,
-               const gchar *key, /* NULL for initialization */
-               gpointer     user_data) /* NULL to get the palette */
+               const gchar *key,       /* NULL for initialization */
+               gpointer     user_data) /* NULL to get the palette to free it */
 {
     static GdkRGBA forecolor, backcolor;
     static GdkRGBA *palette = NULL;
@@ -363,22 +389,26 @@ update_colors (GSettings   *settings,
     if (!user_data)
         return palette;
 
-    if (key == NULL)
+    if (!key)
     {
         g_autofree gchar *backcolor_str = get_setting (settings, BACKCOLOR_KEY);
-        gdk_rgba_parse (&backcolor, backcolor_str);
         g_autofree gchar *forecolor_str = get_setting (settings, FORECOLOR_KEY);
+
+        gdk_rgba_parse (&backcolor, backcolor_str);
         gdk_rgba_parse (&forecolor, forecolor_str);
+
         palette = get_palette(settings, PALETTE_KEY, &palette_size);
     }
     else if (strcmp (key, BACKCOLOR_KEY) == 0)
     {
         g_autofree gchar *backcolor_str = get_setting (settings, BACKCOLOR_KEY);
+
         gdk_rgba_parse (&backcolor, backcolor_str);
     }
     else if (strcmp (key, FORECOLOR_KEY) == 0)
     {
         g_autofree gchar *forecolor_str = get_setting (settings, FORECOLOR_KEY);
+
         gdk_rgba_parse (&forecolor, forecolor_str);
     }
     else if (strcmp (key, PALETTE_KEY) == 0)
@@ -396,9 +426,9 @@ update_colors (GSettings   *settings,
     return NULL;
 }
 
-int
-main(int   argc,
-     char *argv[])
+gint
+main(gint   argc,
+     gchar *argv[])
 {
     g_autoptr (GError) error = NULL;
 
@@ -414,22 +444,21 @@ main(int   argc,
     textdomain(GETTEXT_PACKAGE);
     bindtextdomain(GETTEXT_PACKAGE, LOCALEDIR);
     bind_textdomain_codeset (GETTEXT_PACKAGE, "UTF-8");
-    if (!gtk_init_with_args (&argc,
-                             &argv,
-                             N_(" - minimalist vte-based terminal emulator"),
-                             options,
-                             GETTEXT_PACKAGE,
-                             &error))
+
+    if (!gtk_init_with_args (&argc, &argv, N_(" - minimalist vte-based terminal emulator"), options, GETTEXT_PACKAGE, &error))
     {
         g_critical ("%s", error->message);
         return 1;
     }
 
+    /* Create window */
     GtkWidget *window = gtk_window_new (GTK_WINDOW_TOPLEVEL);
+    GtkWindow *win = GTK_WINDOW (window);
 
     /* Vte settings */
     GtkWidget *terminal = vte_terminal_new ();
     VteTerminal *term = VTE_TERMINAL (terminal);
+
     vte_terminal_set_mouse_autohide      (term, TRUE);
     vte_terminal_set_audible_bell        (term, FALSE);
     vte_terminal_set_scroll_on_output    (term, FALSE);
@@ -441,19 +470,18 @@ main(int   argc,
     gtk_widget_show_all (window);
 
     /* Window settings */
-    gtk_window_set_decorated (GTK_WINDOW (window), FALSE);
-    gtk_window_maximize (GTK_WINDOW (window));
+    gtk_window_set_decorated (win, FALSE);
+    gtk_window_maximize (win);
 
     /* Url matching stuff */
     g_autoptr (GRegex) url_regexp = g_regex_new (URL_REGEXP,
                                                  G_REGEX_CASELESS | G_REGEX_OPTIMIZE,
                                                  G_REGEX_MATCH_NOTEMPTY,
                                                  NULL); /* error */
-    vte_terminal_match_add_gregex (term,
-                                   url_regexp,
-                                   0);
 
-    /* Apply user settings */
+    vte_terminal_match_add_gregex (term, url_regexp, 0);
+
+    /* Apply and track user settings */
     g_autoptr (GSettings) settings = g_settings_new ("org.gnome.Germinal");
 
     SETTING_SIGNAL (BACKCOLOR, colors);
@@ -468,7 +496,7 @@ main(int   argc,
     /* Launch base command */
     g_autofree gchar *cwd = g_get_current_dir ();
 
-    if (G_LIKELY (command == NULL))
+    if (G_LIKELY (!command))
     {
         g_autofree gchar *setting = get_setting (settings, STARTUP_COMMAND_KEY);
         command = g_strsplit (setting , " ", 0);
@@ -478,12 +506,7 @@ main(int   argc,
     g_auto (GStrv) envp = g_environ_setenv (NULL, "TERM", get_setting (settings, TERM_KEY), TRUE);
 
     /* Spawn our command */
-    if (!vte_terminal_spawn_sync (term,
-                                  VTE_PTY_DEFAULT,
-                                  cwd,
-                                  command,
-                                  envp,
-                                  G_SPAWN_SEARCH_PATH,
+    if (!vte_terminal_spawn_sync (term, VTE_PTY_DEFAULT, cwd, command, envp, G_SPAWN_SEARCH_PATH,
                                   NULL, /* child_setup */
                                   NULL, /* child_setup_data */
                                   NULL, /* child_pid */
