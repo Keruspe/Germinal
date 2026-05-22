0c12db427398dd4a6875b496388fe11f41d6d5c6
Marc-Antoine Perennou <Marc-Antoine@Perennou.com>
2026-05-23 00:32:23 +0200

    switch to SPDX
    Signed-off-by: Marc-Antoine Perennou <Marc-Antoine@Perennou.com>

--- src/germinal/germinal-preferences.h
+++ src/germinal/germinal-preferences.h
@@ -1,21 +1,5 @@
-/*
- * This file is part of Germinal.
- *
- * Copyright 2026 Marc-Antoine Perennou <Marc-Antoine@Perennou.com>
- *
- * Germinal is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * Germinal is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with Germinal.  If not, see <http://www.gnu.org/licenses/>.
- */
+// SPDX-FileCopyrightText: 2026 Marc-Antoine Perennou <Marc-Antoine@Perennou.com>
+// SPDX-License-Identifier: GPL-3.0-or-later
 
 #pragma once
 
